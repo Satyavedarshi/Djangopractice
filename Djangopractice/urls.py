54c1349b003d7db7996f414c109124148fe4c87c
saramise <satyavedarshi.ramisetti@oracle.com>
2021-06-10 21:51:30 +0530

    new repo

--- Djangopractice/urls.py
+++ Djangopractice/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from webapp1.tests import sayhello
+
+import webapp1.views
+import webapp1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',sayhello),
-    #path('', include("webapp1.urls")),
+    path('', webapp1.views.greet),
 ]
---
+++ webapp1/urls.py
@@ -0,0 +1,5 @@
+
+from django.http import HttpResponse
+
+def greet(response):
+    return HttpResponse('Hi Developer, Good Morning')
