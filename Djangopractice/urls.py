06391ef858618eae72ba176ec84f1fd7320c6e75
saramise <satyavedarshi.ramisetti@oracle.com>
2021-06-10 18:46:05 +0530

    new repo

--- Djangopractice/urls.py
+++ Djangopractice/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from webapp1.tests import sayhello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("webapp1.urls")),
+    path('',sayhello),
+    #path('', include("webapp1.urls")),
 ]
